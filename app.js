dc720079ef96e299e1854a1f79fccef949cc5f97
aaronmarkle <aaronkmarkle@gmail.com>
2016-01-11 14:53:07 -0800

    you can now switch between chat rooms and only see and submit to the room you are currently in #1

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
+  socket.on('dev message', function(msg){
+    io.emit('dev message', msg);
+  });
+  socket.on('sports message', function(msg){
+    io.emit('sports message', msg);
+  });
+  socket.on('games message', function(msg){
+    io.emit('games message', msg);
+  });
 });
 
 http.listen(8080, function(){
--- default.js
+++ default.js
@@ -1,12 +1,79 @@
 var socket = io();
+
+var currentRoom = 'main';
+
+document.getElementById('main-link').addEventListener('click', function(){
+  document.getElementById(currentRoom).classList.add('hidden');
+  document.getElementById('main').classList.remove('hidden');
+  currentRoom = 'main';
+});
+
+document.getElementById('dev-link').addEventListener('click', function(){
+  document.getElementById(currentRoom).classList.add('hidden');
+  document.getElementById('dev').classList.remove('hidden');
+  currentRoom = 'dev';
+});
+
+document.getElementById('sports-link').addEventListener('click', function(){
+  document.getElementById(currentRoom).classList.add('hidden');
+  document.getElementById('sports').classList.remove('hidden');
+  currentRoom = 'sports';
+});
+
+document.getElementById('games-link').addEventListener('click', function(){
+  document.getElementById(currentRoom).classList.add('hidden');
+  document.getElementById('games').classList.remove('hidden');
+  currentRoom = 'games';
+});
+
 document.getElementById('send').addEventListener('click', function(e){
   e.preventDefault();
   socket.emit('chat message', document.getElementById('m').value);
   document.getElementById('form').reset();
 });
+
+document.getElementById('dev-send').addEventListener('click', function(e){
+  e.preventDefault();
+  socket.emit('dev message', document.getElementById('dev-m').value);
+  document.getElementById('dev-form').reset();
+});
+
+document.getElementById('sports-send').addEventListener('click', function(e){
+  e.preventDefault();
+  socket.emit('sports message', document.getElementById('sports-m').value);
+  document.getElementById('sports-form').reset();
+});
+
+document.getElementById('games-send').addEventListener('click', function(e){
+  e.preventDefault();
+  socket.emit('games message', document.getElementById('games-m').value);
+  document.getElementById('games-form').reset();
+});
+
 socket.on('chat message', function(msg){
   var li = document.createElement('li');
   var node = document.createTextNode(msg);
   li.appendChild(node);
   document.getElementById('messages').appendChild(li);
+});
+
+socket.on('dev message', function(msg){
+  var li = document.createElement('li');
+  var node = document.createTextNode(msg);
+  li.appendChild(node);
+  document.getElementById('dev-messages').appendChild(li);
+});
+
+socket.on('sports message', function(msg){
+  var li = document.createElement('li');
+  var node = document.createTextNode(msg);
+  li.appendChild(node);
+  document.getElementById('sports-messages').appendChild(li);
+});
+
+socket.on('games message', function(msg){
+  var li = document.createElement('li');
+  var node = document.createTextNode(msg);
+  li.appendChild(node);
+  document.getElementById('games-messages').appendChild(li);
 });
